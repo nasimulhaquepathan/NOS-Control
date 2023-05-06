65725b0f35dac98c987e29bc1f1aa508c1513da0
nasimulhaquepathan <nasimul194@gmail.com>
2023-05-06 18:03:19 +0600

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import globalReducer from './state';
+import App from './App.jsx';
+import './index.css';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import {api } from './state/api'
+
+const store = configureStore({
+  reducer: {
+    global : globalReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware)
+})
+
+setupListeners(store.dispatch)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
