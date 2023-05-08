7ef462bd780f7c08bb70ef9b8a59df28a0fbde0b
nasimulhaquepathan <nasimul194@gmail.com>
2023-05-09 01:29:07 +0600

    vite config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,5 +9,16 @@ export default defineConfig({
     define: {
       __VITE_REACT_APP_BASE_URL__: `"${process.env.VITE_REACT_APP_BASE_URL}"`, // wrapping in "" since it's a string
     },
+    build: {
+      rollupOptions: {
+          output:{
+              manualChunks(id) {
+                  if (id.includes('node_modules')) {
+                      return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                  }
+              }
+          }
+      }
+  },
   plugins: [react()],
 })
